ec953e60a7f45babf6e025f0f068954197d5b6cd
Érica Joice dos Santos <ejoice19@gmail.com>
2022-06-15 22:57:14 -0300

    Update script.js

--- script.js
+++ script.js
@@ -9,10 +9,43 @@ const rock = document.getElementById('pedra');
 const paper = document.getElementById('papel');
 const scissors = document.getElementById('tesoura');
 
-//?Funçã para mostrar a escolha do player;
-function game(playerChoice){
-   console.log('<3<3<3  ' + playerChoice)
+//*computador escolhe aletoriamente
+function jogadaComputador(){
+   const choices = ['pedra', 'papel', 'tesoura'];
+   const randomNumber= Math.floor(Math.random() * 3); //* Pega o Elemento
+   return choices[randomNumber]; 
 }
+
+
+
+//?Funçã para mostrar a escolha do player;
+
+function game(playerChoice){
+   const pcChoice = jogadaComputador();
+   console.log("a jogada do pc é" + pcChoice);
+
+   switch(playerChoice + pcChoice) {
+      case "pedratesoura" :
+      case "papelpedra" :
+      case "tesourapapel" :
+         console.log("UserGanhe");
+         break;
+      case "pedrapapel":
+      case "papeltesoura":
+      case "tesourapedra":
+         console.log("user perdeu");
+         break;
+      case "pedrapedra":
+      case "papelpapel":
+      case "tesouratesoura":
+         console.log("User empata");
+         break;
+   }
+
+
+}
+
+
 
 function main(){
 
